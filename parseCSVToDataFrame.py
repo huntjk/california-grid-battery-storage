0f49d8a9f64e625bf79555c2da4f9daf695c6d44
JK Hunt <jkhunt@DN0a1f4334.SUNet>
2018-11-14 21:54:15 -0800

    Added location data to csv per zipcode.

--- parseCSVToDataFrame.py
+++ parseCSVToDataFrame.py
@@ -1,9 +1,16 @@
 import csv
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('output.csv')
-zipcode_location_df = pd.read_csv('US Zip Codes from 2013 Government Data')
+zipcode_df = pd.read_csv('/Users/jkhunt/github/batteries-california/US_Zipcodes.csv', skiprows = range(1, 30022))
 df.columns = ['Zipcode', 'Supply_KW', 'Demand_Month_1_KWh', 'Demand_Month_2_KWh', 'Demand_Month_3_KWh', 'Demand_Month_4_KWh', 'Demand_Month_5_KWh', 'Demand_Month_6_KWh', 'Demand_Month_7_KWh', 'Demand_Month_8_KWh', 'Demand_Month_9_KWh', 'Demand_Month_10_KWh', 'Demand_Month_11_KWh', 'Demand_Month_12_KWh']
-#df = df.append(df.agg(['sum', 'mean']))
-print zipcode_location_df
-print df
+
+zipcode_list = []
+for index, row in df.iterrows():
+    zip_df = zipcode_df.loc[zipcode_df['ZIP'] == row[0]][['LAT', 'LNG']].values.flatten()
+    zipcode_list.append(zip_df)
+
+lat_long_df = pd.DataFrame(zipcode_list, columns = ['Latitude', 'Longitude'])
+df = df.join(lat_long_df)
+df.to_csv('/Users/jkhunt/github/batteries-california/output_with_locations.csv', index = False)

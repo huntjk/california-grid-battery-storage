9d87eaa9ad83014e23d1ceaecaa11a4d2f90a3aa
JK Hunt <jkhunt@DN0a24a7cd.SUNet>
2018-11-15 16:08:01 -0800

    Added config comments

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
 import parseCSVToDataFrame
 
-# Insert any global variables here
+# Insert any global variables here:
+
+# DataFrame containing all data so far.
+# Uses format 'Zipcode', 'Latitude', 'Longitude', 'Supply (KW)', 'Demand per month (KWh)':
+# To iterate through values:
+#
+# for index, row in config.data.iterrows():
+#   zipcode = row['Zipcode']
+#   latitude, longitude = row['Latitude'], row['Longitude']
+#   supply_KW = row['Supply_KW']
+#   demand_month_1_KWh, demand_month_2_KWh = row['Demand_Month_1_KWh'], row['Demand_Month_2_KWh']
+#
+# Can call up to 'Demand_Month_12_KWh'
 data = parseCSVToDataFrame.df

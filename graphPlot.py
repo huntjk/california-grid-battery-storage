1792284a4aa07587c4a0a46a5b71e92bbf953233
JK Hunt <jkhunt@DNa1d41b8.SUNet>
2018-11-16 16:55:05 -0800

    Graphing stuff.

--- graphPlot.py
+++ graphPlot.py
@@ -1,4 +1,4 @@
-import config
+import config as cfg
 import numpy as np
 import folium
 from folium import plugins
@@ -9,17 +9,15 @@ import pandas as pd
 def main():
     folium_map = folium.Map([37.7783, -119.4179], zoom_start = 7)
 
-    print config.test_data_df
-
-    for index, row in config.test_data_df.iterrows():
+    for index, row in cfg.test_data_df.iterrows():
         if np.isnan(row['Latitude']) or np.isnan(row['Longitude']): continue
 
         popup_text = """Zipcode: {}<br>
                         2017 Supply (KW): {}<br>
                         2017 Demand (KWh): {}"""
-        popup_text = popup_text.format(str(index), row['Supply_KW'], 1)
+        popup_text = popup_text.format(str(index), row['Supply_KW'], sum(cfg.test_data[index][cfg.MONTH_1_KWH:]))
 
-        folium.CircleMarker([row['Latitude'], row['Longitude']], radius = 0.001 * row['Supply_KW'], popup = popup_text).add_to(folium_map)
+        folium.CircleMarker([row['Latitude'], row['Longitude']], radius = 0.01 * row['Supply_KW'], popup = popup_text).add_to(folium_map)
     folium_map.save("/Users/jkhunt/github/batteries-california/test_map.html")
 
 if __name__ == '__main__':

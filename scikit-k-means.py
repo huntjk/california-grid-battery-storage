0254210c58a9540af2bb2b57cf040662aedf003d
JK Hunt <jkhunt@DN0a24722c.SUNet>
2018-12-02 15:02:16 -0800

    k-means updates

--- scikit-k-means.py
+++ scikit-k-means.py
@@ -15,25 +15,42 @@ def main():
     for val in data.values():
         zip_coords.append([val[0], val[1]])
         zip_weights.append(val[cfg.month_index])
-    kmeans = KMeans(n_clusters = cfg.n_batteries, init = 'k-means++')
+    kmeans = KMeans(n_clusters = cfg.n_batteries, init = 'k-means++', n_init = 50)
     kmeans.fit(np.array(zip_coords), sample_weight = np.array(zip_weights))
     distribution = label_distribution(kmeans.labels_)
-    print_distribution(distribution)
-    gp.graph_scikit(kmeans.cluster_centers_, zip_coords, distribution)
-    # print kmeans.cluster_centers_
-    # print kmeans.labels_
     # print kmeans.inertia_
-    # print kmeans.n_iter_
+    # print_distribution_info(distribution, zip_weights)
+    # detect_stacked(kmeans.cluster_centers_)
+    gp.graph_scikit(kmeans.cluster_centers_, zip_coords, distribution)
+
+def print_distribution_info(distribution, weights):
+    result = []
+    for index, arr in distribution.items():
+        demands = [weights[i] for i in arr]
+        result.append(sum(demands))
+        # print 'Cluster Mean {}: {}'.format(index, mean)
+    print 'Min: {}, Max: {}, STD: {}, Average: {}'.format(np.min(result), np.max(result), np.std(result), np.mean(result))
 
 def label_distribution(labels):
     result = col.defaultdict(list)
     for index, val in enumerate(labels):
         result[val].append(index)
+    # print_distribution_counts(result)
     return result
 
-def print_distribution(distribution):
+def print_distribution_counts(distribution):
+    result = col.defaultdict(int)
+    total = 0
     for index, arr in distribution.items():
-        print '{}: {}'.format(index, len(arr))
+        result[index] = len(arr)
+        total += len(arr)
+    print result, total
+
+def detect_stacked(centers):
+    result = col.defaultdict(list)
+    for index, val in enumerate(centers):
+        result[tuple(val)].append(index)
+    print len(result)
 
 if __name__ == '__main__':
 	main()
